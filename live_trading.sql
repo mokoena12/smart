959476c018225bbe4322abaee21a72ef10f99475
Mabelane Evidence <evidencemabelane@gmail.com>
2021-12-04 13:56:13 +0200

    Live trading

--- live_trading.sql
+++ live_trading.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2021 at 12:10 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Generation Time: Dec 04, 2021 at 12:52 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,22 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `live_trading` (
   `username` varchar(255) NOT NULL,
-  `currency_type` varchar(20) NOT NULL,
+  `trading_type` varchar(20) NOT NULL,
   `currency_pair` varchar(20) NOT NULL,
-  `lot_size` int(20) NOT NULL,
-  `entry_price` int(20) NOT NULL,
-  `stop_loss` int(20) NOT NULL,
-  `take_profit` int(20) NOT NULL,
-  `action` varchar(20) NOT NULL
+  `lot_size` double(16,5) NOT NULL,
+  `entry_price` double(16,5) NOT NULL,
+  `stop_loss` double(16,5) NOT NULL,
+  `take_profit` double(16,5) NOT NULL,
+  `trading_action` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `live_trading`
 --
 
-INSERT INTO `live_trading` (`username`, `currency_type`, `currency_pair`, `lot_size`, `entry_price`, `stop_loss`, `take_profit`, `action`) VALUES
-('bel', '', 'USDZAR', 12, 5, 3, 20, 'Buy'),
-('Raps', 'Forex', 'USDJPY', 25, 36, 58, 25, 'Sell');
+INSERT INTO `live_trading` (`username`, `trading_type`, `currency_pair`, `lot_size`, `entry_price`, `stop_loss`, `take_profit`, `trading_action`) VALUES
+('Raps', 'Forex', 'USDZAR', 25.00000, 25.00000, 25.00000, 24.00000, 'Sell');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
